345531b949482d4b2778eee98c94abd4e803acbe
David May <49894298+wass3rw3rk@users.noreply.github.com>
2023-10-11 09:13:01 -0500

    chore(license): update source code headers + copyright year (#64)

--- cmd/vela-hugo/main.go
+++ cmd/vela-hugo/main.go
@@ -1,6 +1,4 @@
-// Copyright (c) 2022 Target Brands, Inc. All rights reserved.
-//
-// Use of this source code is governed by the LICENSE file in this repository.
+// SPDX-License-Identifier: Apache-2.0
 
 package main
 
@@ -40,7 +38,7 @@ func main() {
 	app.Name = "vela-hugo"
 	app.HelpName = "vela-hugo"
 	app.Usage = "Vela Hugo plugin for generating a static website"
-	app.Copyright = "Copyright (c) 2022 Target Brands, Inc. All rights reserved."
+	app.Copyright = "Copyright 2021 Target Brands, Inc. All rights reserved."
 	app.Authors = []*cli.Author{
 		{
 			Name:  "Vela Admins",
